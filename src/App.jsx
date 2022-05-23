f21ae83e435ee049e36177818f45c575bf7fa051
Sonu <sonuby93@gmail.com>
2022-05-23 19:17:55 +0530

    New Component added and added styling in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experiences from './components/experience/Experiences'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Nav />
+      <About/>
+      <Experiences/>
+      <Portfolio/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
+    </div>
   )
 }
 

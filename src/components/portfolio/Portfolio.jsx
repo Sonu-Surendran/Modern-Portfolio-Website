e5f6392fa461376a5d3bf77daab6b4b58a65ff32
Sonu <sonuby93@gmail.com>
2022-05-26 08:26:01 +0530

    Portfolio component completed and demo images for portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './portfolio.css'
+import PortfolioItems from './PortfolioItems'
+import IMG1 from '../../images/portfolio1.jpg'
+import IMG2 from '../../images/portfolio2.jpg'
+import IMG3 from '../../images/portfolio3.jpg'
+import IMG4 from '../../images/portfolio4.jpg'
+import IMG5 from '../../images/portfolio5.png'
 
 function Portfolio() {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+         <PortfolioItems image={IMG1} title="The title" link="https://github.com"/>
+         <PortfolioItems image={IMG2} title="The title" link="https://github.com"/>
+         <PortfolioItems image={IMG3} title="The title" link="https://github.com"/>
+         <PortfolioItems image={IMG4} title="The title" link="https://github.com"/>
+         <PortfolioItems image={IMG5} title="The title" link="https://github.com"/>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/portfolio/PortfolioItems.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './portfolio.css'
+
+const PortfolioItems = (property) => {
+  return (
+    <div>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={property.image} alt="" />
+          </div>
+          <h3>{property.title}</h3>
+          <div className="portfolio__item-cta">
+          <a href={property.link} className='btn' target='_blank'>Github</a>
+          </div>
+        </article>
+    </div>
+  )
+}
+
+export default PortfolioItems
